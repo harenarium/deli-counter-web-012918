a00e84c0c386435d0c00c97ce8da8dcaae8fa93a
Caroline Lee <carolinel2610@gmail.com>
2018-01-12 01:09:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ def line(katz_deli)
   end
 end
 
-# 
+#
 # function currentLine(katzDeliLine){
 #   if (katzDeliLine.length === 0){
 #      return "The line is currently empty."
@@ -26,13 +26,13 @@ end
 #   return string
 #   }
 # }
-# 
-# 
+#
+#
 # function takeANumber(katzDeliLine, newName){
 #   katzDeliLine.push(newName)
 #   return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`
 # }
-# 
+#
 # function nowServing(katzDeliLine){
 #   if (katzDeliLine.length>0){
 #     const firstPerson = katzDeliLine.shift()
